3f9ce06aa95cd5481c9ea539c7bbf09e2714353b
aohkgnadnart <83690404+aohkgnadnart@users.noreply.github.com>
2022-04-29 21:19:41 +0700

    Update CustomRecyclerScrollViewListener.java

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/CustomRecyclerScrollViewListener.java
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/CustomRecyclerScrollViewListener.java
@@ -4,30 +4,18 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 public abstract class CustomRecyclerScrollViewListener extends RecyclerView.OnScrollListener {
-    int scrollDist = 0;
-    boolean isVisible = true;
-    static final float MINIMUM = 20;
-
+    int dyBefore = 0;
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
-//        Log.d("OskarSchindler", "Scroll Distance "+scrollDist);
-
-        if (isVisible && scrollDist > MINIMUM) {
-            Log.d("OskarSchindler", "Hide " + scrollDist);
-            hide();
-            scrollDist = 0;
-            isVisible = false;
-        } else if (!isVisible && scrollDist < -MINIMUM) {
-            Log.d("OskarSchindler", "Show " + scrollDist);
+        Log.d("OskarSchindler", "Scroll Distance "+ scrollDist + " " + dx + " " + dy);
+        if(dy - dyBefore > 0){
             show();
-            scrollDist = 0;
-            isVisible = true;
         }
-        if ((isVisible && dy > 0) || (!isVisible && dy < 0)) {
-            Log.d("OskarSchindler", "Add Up " + scrollDist);
-            scrollDist += dy;
+        else{
+            hide();
         }
+        dyBefore = dy;
     }
 
     public abstract void show();
